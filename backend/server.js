6b8af2465851be53f949ae7f9f15a17f4deee28b
krish7x <krishnakumar99g@gmail.com>
2021-07-30 18:45:40 +0530

    server and mongo  integration

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+/**
+ * @author krish
+ */
+
+const express = require("express");
+const mongo = require("./config/mongo");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+//http middlewares
+app.use(express.static("public"));
+app.use(express.json());
+
+//mongo connection
+mongo();
+
+//logger
+var log4js = require("log4js");
+var logger = log4js.getLogger();
+logger.level = "debug";
+
+//cors
+app.use(cors());
+app.use("/api", route);
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  logger.info("Listening on port", PORT);
+});
